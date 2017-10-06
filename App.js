8df6a961da0b27728c4f010f571847845249caa6
Suvesh Agnihotri <suvesh@minance.com>
2017-10-06 18:20:36 +0530

    navigator added

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-import { StyleSheet } from 'react-native';
+import {StyleSheet,Text,} from 'react-native';
 import TodoList from "./TodoList";
-
+import NavigationExperimental from 'react-native-deprecated-custom-components';
 class App extends React.Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state ={
+        this.state = {
             todos: [
                 {
                     tasks: 'Suvesh Agnihotri',
@@ -37,27 +37,47 @@ class App extends React.Component {
         };
     }
 
-
-    onAddStarted(){
-     console.log('onAdd Clicked');
+    renderScene(route, nav) {
+        switch (route.name) {
+            case 'taskform':
+                return (
+                    <Text>Add New Task Come Here</Text>
+                );
+            default:
+                return (
+                    <TodoList
+                        onAddStarted={this.onAddStarted.bind(this)}
+                        todos={this.state.todos}
+                    />
+                );
+        }
     }
 
-  render() {
-    return (
-      <TodoList
-          onAddStarted={this.onAddStarted.bind(this)}
-          todos={this.state.todos}/>
-    );
-  }
+    onAddStarted() {
+        this.nav.push({
+            name : 'taskform',
+        });
+    }
+
+    render() {
+        return (
+            <NavigationExperimental initialRoute={{name: 'taskList', index: 0}}
+                       ref={((nav)=>{
+                           this.nav = nav;
+                       })}
+                       renderScene={this.renderScene.bind(this)}
+            />
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
 
-export  default App;
+export default App;
